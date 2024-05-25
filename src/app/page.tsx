d4e79074441ac87ef205cee9f3b535002b2d2758
Md. Sefat Alam <44186213+Md-Sefat-Alam@users.noreply.github.com>
2024-05-25 21:13:16 +0600

    Add Food Item

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
 import FoodItem from "@/components/FoodItem";
+import { Button } from "@mui/material";
 
 export default function Home() {
   return (
     <div className="container mx-auto p-4">
+      <Button
+        variant="contained"
+        color="primary"
+        //  onClick={handleAdd}
+      >
+        Add Food Item
+      </Button>
       <FoodItem />
     </div>
   );
